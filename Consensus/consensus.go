a4ab807f84f62932e2fb19cd0d700ab57079357e
Aimen Safdar <i210588@nu.edu.pk>
2024-12-19 03:15:46 +0500

    added validator file

--- Consensus/consensus.go
+++ Consensus/consensus.go
@@ -147,27 +147,4 @@ func (cm *ConsensusManager) GetNodeBlockchain(nodeID string) (*blockchain.Blockc
     return blockchain, nil
 }
 
-type NetworkBlockchainValidator struct {
-    consensusManager *ConsensusManager
-}
 
-func NewNetworkBlockchainValidator(difficulty int) *NetworkBlockchainValidator {
-    return &NetworkBlockchainValidator{
-        consensusManager: NewConsensusManager(difficulty),
-    }
-}
-
-func (nbv *NetworkBlockchainValidator) ValidateIncomingBlockchain(nodeID string, blockchainData []byte) error {
-    return nbv.consensusManager.ValidateAndSyncBlockchain(nodeID, blockchainData)
-}
-
-func (nbv *NetworkBlockchainValidator) HandleBlockchainSync(nodeID string, receivedBlockchain []byte) error {
-    fmt.Printf("[CONSENSUS] Handling blockchain sync for node %s\n", nodeID)
-    err := nbv.ValidateIncomingBlockchain(nodeID, receivedBlockchain)
-    if err != nil {
-        fmt.Printf("[CONSENSUS ERROR] Blockchain sync failed: %v\n", err)
-        return fmt.Errorf("blockchain sync failed: %v", err)
-    }
-    fmt.Printf("[CONSENSUS] Blockchain sync successful for node %s\n", nodeID)
-    return nil
-}
